2a5f884c5c6206d929afa4b9b48052005b1df847
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-11-19 15:21:31 +0100

    tried a thing

--- sydtest/src/Test/Syd.hs
+++ sydtest/src/Test/Syd.hs
@@ -16,6 +16,7 @@ import qualified Data.UUID.V4 as UUID
 import Foreign.StablePtr
 import GHC.Generics (Generic)
 import System.Exit
+import System.IO (hFlush)
 import System.Posix.Files
 import System.Posix.IO
 import System.Posix.Process
@@ -35,16 +36,8 @@ instance ToJSON TestStatus
 
 runTest :: Test -> IO TestRunResult
 runTest func = do
-  -- FIXME: Forking a process is expensive and potentially not needed for pure code.
-  uuid <- UUID.nextRandom
-  sharedMemOutsideFd <- shmOpen (UUID.toString uuid) (ShmOpenFlags {shmReadWrite = False, shmCreate = True, shmExclusive = True, shmTrunc = True}) ownerReadMode
-  sharedMemOutsideHandle <- fdToHandle sharedMemOutsideFd
-  writeVar <- newEmptyMVar
-  readVar <- newEmptyMVar
-  testProcess <- forkProcess $ do
-    sharedMemInsideFd <- shmOpen (UUID.toString uuid) (ShmOpenFlags {shmReadWrite = True, shmCreate = False, shmExclusive = False, shmTrunc = False}) ownerWriteMode
-    sharedMemInsideHandle <- fdToHandle sharedMemInsideFd
-    status <-
+  testRunResultStatus <-
+    runInProcess $
       (func >>= (evaluate . (`seq` TestPassed)))
         `catches` [ Handler $ \(_ :: ErrorCall) -> pure TestFailed,
                     Handler $ \(_ :: ExitCode) -> pure TestFailed,
@@ -53,17 +46,45 @@ runTest func = do
                     Handler $ \(_ :: RecUpdError) -> pure TestFailed,
                     Handler $ \(_ :: PatternMatchFail) -> pure TestFailed
                   ]
-    LB.hPut sharedMemInsideHandle $ JSON.encode status
+  pure TestRunResult {..}
+
+runInProcess :: (FromJSON a, ToJSON a) => IO a -> IO a
+runInProcess func = do
+  -- FIXME: Forking a process is expensive and potentially not needed for pure code.
+  uuid <- UUID.nextRandom
+  print uuid
+  (sharedMemOutsideFd, sharedMemInsideFd) <- createPipe
+  -- sharedMemOutsideFd <- shmOpen (UUID.toString uuid) (ShmOpenFlags {shmReadWrite = True, shmCreate = True, shmExclusive = False, shmTrunc = True}) ownerModes
+  sharedMemOutsideHandle <- fdToHandle sharedMemOutsideFd
+  writeVar <- newEmptyMVar
+  readVar <- newEmptyMVar
+  print "Creating test process"
+  testProcess <- forkProcess $ do
+    sharedMemInsideHandle <- fdToHandle sharedMemInsideFd
+    print "starting test"
+    status <- func
+    print "test done"
+    let statusBs = LB.toStrict $ JSON.encode status
+    print statusBs
+    hPut sharedMemInsideHandle statusBs
+    hFlush sharedMemInsideHandle
+    print "written"
     putMVar writeVar ()
+    print "test waiting to be read"
     -- Wait for the status to be read
     takeMVar readVar
-  () <- takeMVar readVar
-  statusBs <- LB.hGetContents sharedMemOutsideHandle
+    print "test process done"
+  print "outer waiting to read"
+  () <- takeMVar writeVar
+  statusBs <- LB.fromStrict <$> hGetContents sharedMemOutsideHandle
+  print statusBs
+  print "read"
+  putMVar readVar ()
   case JSON.eitherDecode statusBs of
     Left err -> die err
-    Right testRunResultStatus -> do
+    Right res -> do
       putMVar readVar ()
       print "read"
       ps <- getProcessStatus True False testProcess
       print ps
-      pure TestRunResult {..}
+      pure res

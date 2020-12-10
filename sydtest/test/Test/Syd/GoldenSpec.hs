cad035f4dbee65f28a0a098c2b945d1a4c6bf71b
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-12-10 15:06:01 +0100

    a completed manual golden test

--- sydtest/test/Test/Syd/GoldenSpec.hs
+++ sydtest/test/Test/Syd/GoldenSpec.hs
@@ -4,6 +4,8 @@ module Test.Syd.GoldenSpec (spec) where
 
 import qualified Data.ByteString as SB
 import qualified Data.ByteString.Char8 as SB8
+import Path
+import Path.IO
 import Rainbow
 import Test.Syd
 
@@ -11,6 +13,12 @@ spec :: Spec
 spec = do
   describe "outputResultForest" $
     it "outputs the same as last time" $ do
-      golden <- SB.readFile "test_resources/output.golden"
+      let goldenFileName = "test_resources/output.golden"
+      goldenFile <- resolveFile' goldenFileName
+      mGolden <- forgivingAbsence $ SB.readFile $ fromAbsFile goldenFile
       let actual = SB8.intercalate "\n" $ map SB.concat $ outputSpecForestByteString toByteStringsColors256 (Timed [] 0)
-      actual `shouldBe` golden
+      case mGolden of
+        Nothing -> do
+          ensureDir $ parent goldenFile
+          SB.writeFile (fromAbsFile goldenFile) actual
+        Just golden -> actual `shouldBe` golden

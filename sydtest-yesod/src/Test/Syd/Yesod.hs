f2a6e56b047c7cfd700ff8d05484a90fbe334be9
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-12-15 00:36:19 +0100

    little pieces of docs

--- sydtest-yesod/src/Test/Syd/Yesod.hs
+++ sydtest-yesod/src/Test/Syd/Yesod.hs
@@ -5,6 +5,7 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
 {-# OPTIONS_GHC -fno-warn-redundant-constraints #-}
 
 module Test.Syd.Yesod
@@ -40,12 +41,15 @@ import Network.Wai.Handler.Warp as Warp
 import Test.Syd
 import Yesod.Core as Yesod
 
+-- | Using a function that supplies a 'site', run a test suite.
 yesodSpec :: YesodDispatch site => ((site -> IO ()) -> IO ()) -> YesodSpec site -> Spec
 yesodSpec func = yesodSpecWith (\f () -> func f)
 
+-- | Using a function that supplies a 'site', based on an inner resource, run a test suite.
 yesodSpecWith :: YesodDispatch site => ((site -> IO ()) -> (a -> IO ())) -> YesodSpec site -> SpecWith a
 yesodSpecWith func = aroundWith func . beforeAll (newManager defaultManagerSettings) . aroundWith' yesodSpecWithFunc
 
+-- | Turn a function that takes a 'YesodClient site' into a function that only takes a 'site'.
 yesodSpecWithFunc :: YesodDispatch site => (HTTP.Manager -> YesodClient site -> IO ()) -> (HTTP.Manager -> site -> IO ())
 yesodSpecWithFunc func man site =
   Warp.testWithApplication (Yesod.toWaiAppPlain site) $ \port ->
@@ -57,23 +61,31 @@ yesodSpecWithFunc func man site =
             }
      in func man client
 
-type YesodSpec site = forall l. HContains l HTTP.Manager => TestDefM l (YesodClient site) ()
+-- | For backward compatibility with yesod-test
+type YesodSpec site = forall l. TestDefM (HTTP.Manager ': l) (YesodClient site) ()
 
+-- | A client environment to call a Yesod app.
 data YesodClient site = YesodClient
   { yesodClientSite :: site,
     yesodClientManager :: HTTP.Manager,
     yesodClientSitePort :: Int
   }
 
+-- | A monad to call a Yesod app.
+--
+-- This has access to a 'YesodClient site'.
 newtype YesodClientM site a = YesodClientM {unYesodClientM :: ReaderT (YesodClient site) IO a}
   deriving (Functor, Applicative, Monad, MonadIO, MonadReader (YesodClient site))
 
+-- | Run a YesodClientM site using a YesodClient site
 runYesodClientM :: YesodClient site -> YesodClientM site a -> IO a
 runYesodClientM cenv (YesodClientM rFunc) = runReaderT rFunc cenv
 
+-- | Define a test in the 'YesodClientM site' monad instead of 'IO'.
 yit :: forall site. HasCallStack => String -> YesodClientM site () -> YesodSpec site
 yit s f = it s ((\cenv -> runYesodClientM cenv f) :: YesodClient site -> IO ())
 
+-- | For backward compatibility
 ydescribe :: String -> YesodSpec site -> YesodSpec site
 ydescribe = describe
 

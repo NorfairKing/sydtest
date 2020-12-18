f5234187991e6179ee1a8e4971eb241ed9ea578f
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-12-18 15:29:35 +0100

    refactored out the definition module

--- sydtest-yesod/src/Test/Syd/Yesod.hs
+++ sydtest-yesod/src/Test/Syd/Yesod.hs
@@ -82,64 +82,10 @@ import Network.HTTP.Types as HTTP
 import Network.Wai.Handler.Warp as Warp
 import Test.Syd
 import Test.Syd.Yesod.Client
+import Test.Syd.Yesod.Def
 import Web.Cookie as Cookie
 import Yesod.Core as Yesod
 import Yesod.Core.Unsafe
-
--- | Run a test suite using the given 'site'.
---
--- If your 'site' contains any resources that need to be set up, you probably want to be using one of the following functions instead.
---
--- This function exists for backward compatibility with yesod-test.
-yesodSpec :: YesodDispatch site => site -> YesodSpec site -> Spec
-yesodSpec site = yesodSpecWithSiteGenerator $ pure site
-
--- | Run a test suite using the given 'site' generator.
---
--- If your 'site' contains any resources that you will want to have set up beforhand, you will probably want to use 'yesodSpecWithSiteGeneratorAndArgument' or 'yesodSpecWithSiteSupplierWith' instead.
---
--- This function exists for backward compatibility with yesod-test.
-yesodSpecWithSiteGenerator :: YesodDispatch site => IO site -> YesodSpec site -> Spec
-yesodSpecWithSiteGenerator siteGen = yesodSpecWithSiteGeneratorAndArgument $ \() -> siteGen
-
--- | Run a test suite using the given 'site' generator which uses an inner resource.
---
--- If your 'site' contains any resources that you need to set up using a 'withX' function, you will want to use `yesodSpecWithSiteSupplier` instead.
---
--- This function exists for backward compatibility with yesod-test.
-yesodSpecWithSiteGeneratorAndArgument :: YesodDispatch site => (a -> IO site) -> YesodSpec site -> SpecWith a
-yesodSpecWithSiteGeneratorAndArgument func = yesodSpecWithSiteSupplierWith $ \f a -> func a >>= f
-
--- | Using a function that supplies a 'site', run a test suite.
-yesodSpecWithSiteSupplier :: YesodDispatch site => ((site -> IO ()) -> IO ()) -> YesodSpec site -> Spec
-yesodSpecWithSiteSupplier func = yesodSpecWithSiteSupplierWith (\f () -> func f)
-
--- | Using a function that supplies a 'site', based on an inner resource, run a test suite.
-yesodSpecWithSiteSupplierWith :: YesodDispatch site => ((site -> IO ()) -> (a -> IO ())) -> YesodSpec site -> SpecWith a
-yesodSpecWithSiteSupplierWith func = aroundWith func . beforeAll (newManager defaultManagerSettings) . aroundWith' yesodSpecWithFunc
-
--- | Turn a function that takes a 'YesodClient site' into a function that only takes a 'site'.
-yesodSpecWithFunc :: YesodDispatch site => (HTTP.Manager -> YesodClient site -> IO ()) -> (HTTP.Manager -> site -> IO ())
-yesodSpecWithFunc func man site =
-  Warp.testWithApplication (Yesod.toWaiAppPlain site) $ \port ->
-    let client =
-          YesodClient
-            { yesodClientManager = man,
-              yesodClientSite = site,
-              yesodClientSitePort = port
-            }
-     in func man client
-
--- | For backward compatibility with yesod-test
-type YesodSpec site = TestDefM '[HTTP.Manager] (YesodClient site) ()
-
--- | Define a test in the 'YesodClientM site' monad instead of 'IO'.
-yit :: forall site. HasCallStack => String -> YesodClientM site () -> YesodSpec site
-yit s f = it s ((\cenv -> runYesodClientM cenv f) :: YesodClient site -> IO ())
-
--- | For backward compatibility
-ydescribe :: String -> YesodSpec site -> YesodSpec site
-ydescribe = describe
 
 -- | Make a @GET@ request for the given route
 get :: (Yesod site, RedirectUrl site url) => url -> YesodClientM site ()
---
+++ sydtest-yesod/src/Test/Syd/Yesod/Def.hs
@@ -0,0 +1,89 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# OPTIONS_GHC -fno-warn-redundant-constraints #-}
+
+module Test.Syd.Yesod.Def where
+
+import qualified Blaze.ByteString.Builder as Builder
+import Control.Monad.Catch
+import Control.Monad.Reader
+import Control.Monad.State (MonadState, StateT (..), evalStateT, execStateT)
+import qualified Control.Monad.State as State
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as SB8
+import qualified Data.ByteString.Lazy as LB
+import qualified Data.ByteString.Lazy.Char8 as LB8
+import Data.CaseInsensitive (CI)
+import qualified Data.CaseInsensitive as CI
+import Data.Map (Map)
+import qualified Data.Map as M
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as TE
+import GHC.Stack
+import Network.HTTP.Client as HTTP
+import qualified Network.HTTP.Client as HTTP
+import Network.HTTP.Types as HTTP
+import Network.Wai.Handler.Warp as Warp
+import Test.Syd
+import Test.Syd.Yesod.Client
+import Web.Cookie as Cookie
+import Yesod.Core as Yesod
+import Yesod.Core.Unsafe
+
+-- | Run a test suite using the given 'site'.
+--
+-- If your 'site' contains any resources that need to be set up, you probably want to be using one of the following functions instead.
+--
+-- This function exists for backward compatibility with yesod-test.
+yesodSpec :: YesodDispatch site => site -> YesodSpec site -> Spec
+yesodSpec site = yesodSpecWithSiteGenerator $ pure site
+
+-- | Run a test suite using the given 'site' generator.
+--
+-- If your 'site' contains any resources that you will want to have set up beforhand, you will probably want to use 'yesodSpecWithSiteGeneratorAndArgument' or 'yesodSpecWithSiteSupplierWith' instead.
+--
+-- This function exists for backward compatibility with yesod-test.
+yesodSpecWithSiteGenerator :: YesodDispatch site => IO site -> YesodSpec site -> Spec
+yesodSpecWithSiteGenerator siteGen = yesodSpecWithSiteGeneratorAndArgument $ \() -> siteGen
+
+-- | Run a test suite using the given 'site' generator which uses an inner resource.
+--
+-- If your 'site' contains any resources that you need to set up using a 'withX' function, you will want to use `yesodSpecWithSiteSupplier` instead.
+--
+-- This function exists for backward compatibility with yesod-test.
+yesodSpecWithSiteGeneratorAndArgument :: YesodDispatch site => (a -> IO site) -> YesodSpec site -> SpecWith a
+yesodSpecWithSiteGeneratorAndArgument func = yesodSpecWithSiteSupplierWith $ \f a -> func a >>= f
+
+-- | Using a function that supplies a 'site', run a test suite.
+yesodSpecWithSiteSupplier :: YesodDispatch site => ((site -> IO ()) -> IO ()) -> YesodSpec site -> Spec
+yesodSpecWithSiteSupplier func = yesodSpecWithSiteSupplierWith (\f () -> func f)
+
+-- | Using a function that supplies a 'site', based on an inner resource, run a test suite.
+yesodSpecWithSiteSupplierWith :: YesodDispatch site => ((site -> IO ()) -> (a -> IO ())) -> YesodSpec site -> SpecWith a
+yesodSpecWithSiteSupplierWith func = aroundWith func . beforeAll (newManager defaultManagerSettings) . aroundWith' yesodSpecWithFunc
+
+-- | Turn a function that takes a 'YesodClient site' into a function that only takes a 'site'.
+yesodSpecWithFunc :: YesodDispatch site => (HTTP.Manager -> YesodClient site -> IO ()) -> (HTTP.Manager -> site -> IO ())
+yesodSpecWithFunc func man site =
+  Warp.testWithApplication (Yesod.toWaiAppPlain site) $ \port ->
+    let client =
+          YesodClient
+            { yesodClientManager = man,
+              yesodClientSite = site,
+              yesodClientSitePort = port
+            }
+     in func man client
+
+-- | For backward compatibility with yesod-test
+type YesodSpec site = TestDefM '[HTTP.Manager] (YesodClient site) ()
+
+-- | Define a test in the 'YesodClientM site' monad instead of 'IO'.
+yit :: forall site. HasCallStack => String -> YesodClientM site () -> YesodSpec site
+yit s f = it s ((\cenv -> runYesodClientM cenv f) :: YesodClient site -> IO ())
+
+-- | For backward compatibility
+ydescribe :: String -> YesodSpec site -> YesodSpec site
+ydescribe = describe

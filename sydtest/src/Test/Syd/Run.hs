c3a65b05a51d0bd579877be90dae32836b03bb2a
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-12-26 01:10:49 +0100

    wrap around each example instead of the entire property

--- sydtest/src/Test/Syd/Run.hs
+++ sydtest/src/Test/Syd/Run.hs
@@ -13,6 +13,7 @@ module Test.Syd.Run where
 import Control.Concurrent
 import Control.Exception
 import Control.Monad.IO.Class
+import Data.IORef
 import Data.Map (Map)
 import qualified Data.Map as M
 import Data.Typeable
@@ -20,7 +21,10 @@ import Data.Word
 import GHC.Clock (getMonotonicTimeNSec)
 import GHC.Generics (Generic)
 import Test.QuickCheck
+import Test.QuickCheck.Gen
 import Test.QuickCheck.IO ()
+import Test.QuickCheck.Property hiding (Result (..))
+import qualified Test.QuickCheck.Property as QCP
 import Test.QuickCheck.Random
 import Text.Printf
 
@@ -145,7 +149,7 @@ runPropertyTestWithArg ::
   ((outerArgs -> innerArg -> IO ()) -> IO ()) ->
   IO TestRunResult
 runPropertyTestWithArg p TestRunSettings {..} wrapper = do
-  let args =
+  let qcargs =
         stdArgs
           { replay = Just (mkQCGen testRunSettingSeed, 0),
             chatty = False,
@@ -154,69 +158,66 @@ runPropertyTestWithArg p TestRunSettings {..} wrapper = do
             maxSize = testRunSettingMaxSize,
             maxShrinks = testRunSettingMaxShrinks
           }
-  result <-
-    applyWrapper2 wrapper $ \outerArgs innerArg ->
-      quickCheckWithResult args (p outerArgs innerArg) >>= evaluate
+  qcr <- quickCheckWithResult qcargs (aroundProperty wrapper p)
   let testRunResultGoldenCase = Nothing
-  case result of
-    Left ex -> do
-      let testRunResultStatus = TestFailed
-      let testRunResultException = Just ex
-      let testRunResultNumTests = Nothing
+  let testRunResultNumTests = Just $ fromIntegral $ numTests qcr
+  case qcr of
+    Success {} -> do
+      let testRunResultStatus = TestPassed
+      let testRunResultException = Nothing
       let testRunResultNumShrinks = Nothing
       let testRunResultFailingInputs = []
       let testRunResultExtraInfo = Nothing
-      let testRunResultLabels = Nothing
-      let testRunResultClasses = Nothing
+      let testRunResultLabels = Just $ labels qcr
+      let testRunResultClasses = Just $ classes qcr
+      let testRunResultTables = Just $ tables qcr
+      pure TestRunResult {..}
+    GaveUp {} -> do
+      let testRunResultStatus = TestFailed
+      let testRunResultException = Nothing
+      let testRunResultNumShrinks = Nothing
+      let testRunResultFailingInputs = []
+      let testRunResultExtraInfo = Just $ printf "Gave up, %d discarded tests" (numDiscarded qcr)
+      let testRunResultLabels = Just $ labels qcr
+      let testRunResultClasses = Just $ classes qcr
+      let testRunResultTables = Just $ tables qcr
+      pure TestRunResult {..}
+    Failure {} -> do
+      let testRunResultStatus = TestFailed
+      let testRunResultException = do
+            se <- theException qcr
+            pure $ case fromException se of
+              Just a -> Right a
+              Nothing -> Left $ displayException se
+      let testRunResultNumShrinks = Just $ fromIntegral $ numShrinks qcr
+      let testRunResultFailingInputs = failingTestCase qcr
+      let testRunResultExtraInfo = Nothing
+      let testRunResultLabels = Just $ M.singleton (failingLabels qcr) 1
+      let testRunResultClasses = Just $ M.fromSet (const 1) (failingClasses qcr)
       let testRunResultTables = Nothing
       pure TestRunResult {..}
-    Right qcr -> do
-      let testRunResultNumTests = Just $ fromIntegral $ numTests qcr
-      case qcr of
-        Success {} -> do
-          let testRunResultStatus = TestPassed
-          let testRunResultException = Nothing
-          let testRunResultNumShrinks = Nothing
-          let testRunResultFailingInputs = []
-          let testRunResultExtraInfo = Nothing
-          let testRunResultLabels = Just $ labels qcr
-          let testRunResultClasses = Just $ classes qcr
-          let testRunResultTables = Just $ tables qcr
-          pure TestRunResult {..}
-        GaveUp {} -> do
-          let testRunResultStatus = TestFailed
-          let testRunResultException = Nothing
-          let testRunResultNumShrinks = Nothing
-          let testRunResultFailingInputs = []
-          let testRunResultExtraInfo = Just $ printf "Gave up, %d discarded tests" (numDiscarded qcr)
-          let testRunResultLabels = Just $ labels qcr
-          let testRunResultClasses = Just $ classes qcr
-          let testRunResultTables = Just $ tables qcr
-          pure TestRunResult {..}
-        Failure {} -> do
-          let testRunResultStatus = TestFailed
-          let testRunResultException = do
-                se <- theException qcr
-                pure $ case fromException se of
-                  Just a -> Right a
-                  Nothing -> Left $ displayException se
-          let testRunResultNumShrinks = Just $ fromIntegral $ numShrinks qcr
-          let testRunResultFailingInputs = failingTestCase qcr
-          let testRunResultExtraInfo = Nothing
-          let testRunResultLabels = Just $ M.singleton (failingLabels qcr) 1
-          let testRunResultClasses = Just $ M.fromSet (const 1) (failingClasses qcr)
-          let testRunResultTables = Nothing
-          pure TestRunResult {..}
-        NoExpectedFailure {} -> do
-          let testRunResultStatus = TestFailed
-          let testRunResultException = Nothing
-          let testRunResultNumShrinks = Nothing
-          let testRunResultFailingInputs = []
-          let testRunResultLabels = Just $ labels qcr
-          let testRunResultClasses = Just $ classes qcr
-          let testRunResultTables = Just $ tables qcr
-          let testRunResultExtraInfo = Just $ printf "Expected the property to fail but it didn't."
-          pure TestRunResult {..}
+    NoExpectedFailure {} -> do
+      let testRunResultStatus = TestFailed
+      let testRunResultException = Nothing
+      let testRunResultNumShrinks = Nothing
+      let testRunResultFailingInputs = []
+      let testRunResultLabels = Just $ labels qcr
+      let testRunResultClasses = Just $ classes qcr
+      let testRunResultTables = Just $ tables qcr
+      let testRunResultExtraInfo = Just $ printf "Expected the property to fail but it didn't."
+      pure TestRunResult {..}
+
+aroundProperty :: ((a -> b -> IO ()) -> IO ()) -> (a -> b -> Property) -> Property
+aroundProperty action p = MkProperty . MkGen $ \r n -> aroundProp action $ \a b -> (unGen . unProperty $ p a b) r n
+
+aroundProp :: ((a -> b -> IO ()) -> IO ()) -> (a -> b -> Prop) -> Prop
+aroundProp action p = MkProp $ aroundRose action (\a b -> unProp $ p a b)
+
+aroundRose :: ((a -> b -> IO ()) -> IO ()) -> (a -> b -> Rose QCP.Result) -> Rose QCP.Result
+aroundRose action r = ioRose $ do
+  ref <- newIORef (return QCP.succeeded)
+  action $ \a b -> reduceRose (r a b) >>= writeIORef ref
+  readIORef ref
 
 data GoldenTest a = GoldenTest
   { goldenTestRead :: IO (Maybe a),

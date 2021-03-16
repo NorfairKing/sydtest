8549e429cb6f7f0852096a52a0a5b508f815ca20
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-03-16 09:14:35 +0100

    intermediate commit

--- sydtest-amqp/src/Test/Syd/AMQP.hs
+++ sydtest-amqp/src/Test/Syd/AMQP.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TypeOperators #-}
 
@@ -6,19 +8,25 @@ module Test.Syd.AMQP where
 
 import Control.Concurrent
 import Control.Concurrent.Async
+import Control.Concurrent.STM
 import Control.Exception
+import Data.Aeson as JSON
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Lazy as LB
+import Data.Text (Text)
+import GHC.Generics (Generic)
 import Network.AMQP as AMQP
 import Network.Socket
 import Network.Socket.Free
 import qualified Network.Socket.Wait as Socket
 import Path
 import Path.IO
-import System.Environment
-import System.Process
+import System.Environment (getEnvironment)
+import System.Process.Typed
 import Test.Syd
 
 data RabbitMQHandle = RabbitMQHandle
-  { rabbitMQHandleProcessHandle :: !ProcessHandle,
+  { rabbitMQHandleProcessHandle :: !(Process () () ()),
     rabbitMQHandlePort :: !PortNumber,
     rabbitMQHandleDistributionPort :: !PortNumber
   }
@@ -27,9 +35,10 @@ amqpSpec :: TestDefM (RabbitMQHandle ': outers) AMQP.Connection result -> TestDe
 amqpSpec = rabbitMQSpec . setupAroundWith' amqpConnectionSetupFunc
 
 amqpConnectionSetupFunc :: RabbitMQHandle -> SetupFunc () Connection
-amqpConnectionSetupFunc RabbitMQHandle {..} =
+amqpConnectionSetupFunc h = do
+  liftIO $ cleanRabbitMQState h
   makeSimpleSetupFunc $ \func -> do
-    let opts = defaultConnectionOpts {coServers = [("localhost", rabbitMQHandlePort)]}
+    let opts = defaultConnectionOpts {coServers = [("localhost", rabbitMQHandlePort h)]}
     let acquire = openConnection'' opts
     let release = closeConnection
     let use = func
@@ -67,15 +76,13 @@ rabbitMQServerSetupFunc' = wrapSetupFunc $ \td -> do
           ("RABBITMQ_DIST_PORT", show distPortInt)
         ]
           ++ oldEnv
-  let cp = (proc "rabbitmq-server" []) {env = Just e}
+  let pc = setStdout inherit $ setStderr inherit $ setEnv e $ (proc "rabbitmq-server" [])
   ph <-
     makeSimpleSetupFunc
-      ( \func -> withCreateProcess cp $ \inh outh errh ph -> do
+      ( \func -> withProcessWait pc $ \ph -> do
           Socket.wait "127.0.0.1" portInt
           putStrLn "RabbitMQ ready for testing!"
-          r <- func ph
-          cleanupProcess (inh, outh, errh, ph)
-          pure r
+          func ph
       )
   let pn = fromIntegral portInt -- (hopefully) safe because it came from 'getFreePort'.
   let dpn = fromIntegral distPortInt -- (hopefully) safe because it came from 'getFreePort'.
@@ -85,3 +92,25 @@ rabbitMQServerSetupFunc' = wrapSetupFunc $ \td -> do
         rabbitMQHandlePort = pn,
         rabbitMQHandleDistributionPort = dpn
       }
+
+cleanRabbitMQState :: RabbitMQHandle -> IO ()
+cleanRabbitMQState RabbitMQHandle {..} = do
+  lb <- readProcessStdout_ $ shell "rabbitmqctl list_queues --formatter json"
+  case JSON.eitherDecode lb of
+    Left err -> fail err
+    Right r -> print (r :: ListQueuesOutput)
+
+newtype ListQueuesOutput = ListQueuesOutput [QueueOutput]
+  deriving (Show, Eq, Generic)
+
+instance FromJSON ListQueuesOutput where
+  parseJSON v = ListQueuesOutput <$> parseJSON v
+
+data QueueOutput = QueueOutput
+  { queueOutputName :: Text,
+    queueOutputMessages :: Int
+  }
+  deriving (Show, Eq, Generic)
+
+instance FromJSON QueueOutput where
+  parseJSON = withObject "QueueOutput" $ \o -> QueueOutput <$> o .: "name" <*> o .: "messages"

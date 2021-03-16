62a6bd5b2b19694378c1ee16e7571331a8b3ebd9
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-03-16 08:58:46 +0100

    failing test because of state that is kept

--- sydtest-amqp/test/Test/Syd/AMQPSpec.hs
+++ sydtest-amqp/test/Test/Syd/AMQPSpec.hs
@@ -12,27 +12,66 @@ spec = do
     describe "rabbitMQSpec" $
       it "can run the server and clean up nicely" $ do
         pure () :: IO ()
-  amqpSpec $ do
-    describe "amqpSpec" $
-      it "can write and read a message" $ \conn -> do
-        chan <- openChannel conn
+  amqpSpec $
+    doNotRandomiseExecutionOrder $ do
+      describe "amqpSpec" $ do
+        it "can write and read a message" $ \conn -> do
+          chan <- openChannel conn
 
-        -- declare a queue, exchange and binding
-        declareQueue chan newQueue {queueName = "myQueue"}
-        declareExchange chan newExchange {exchangeName = "myExchange", exchangeType = "direct"}
-        bindQueue chan "myQueue" "myExchange" "myKey"
+          -- declare a queue, exchange and binding
+          _ <- declareQueue chan newQueue {queueName = "myQueue"}
+          declareExchange chan newExchange {exchangeName = "myExchange", exchangeType = "direct"}
+          bindQueue chan "myQueue" "myExchange" "myKey"
 
-        -- publish a message to our new exchange
-        publishMsg
-          chan
-          "myExchange"
-          "myKey"
-          newMsg
-            { msgBody = "hello world",
-              msgDeliveryMode = Just Persistent
-            }
+          -- publish a message to our new exchange
+          let body = "hello world"
+          _ <-
+            publishMsg
+              chan
+              "myExchange"
+              "myKey"
+              newMsg
+                { msgBody = body,
+                  msgDeliveryMode = Just Persistent
+                }
 
-        mMesg <- getMsg chan Ack "myQueue"
-        case mMesg of
-          Nothing -> expectationFailure "Should have received a message"
-          Just (_, _) -> pure ()
+          mMesg <- getMsg chan Ack "myQueue"
+          case mMesg of
+            Nothing -> expectationFailure "Should have received a message"
+            Just (m, e) -> do
+              msgBody m `shouldBe` body
+              ackEnv e
+        it "can write a message (to make sure the next test cannot read it)" $ \conn -> do
+          chan <- openChannel conn
+
+          -- declare a queue, exchange and binding
+          _ <- declareQueue chan newQueue {queueName = "myQueue"}
+          declareExchange chan newExchange {exchangeName = "myExchange", exchangeType = "direct"}
+          bindQueue chan "myQueue" "myExchange" "myKey"
+
+          -- publish a message to our new exchange
+          let body = "hello world"
+          _ <-
+            publishMsg
+              chan
+              "myExchange"
+              "myKey"
+              newMsg
+                { msgBody = body,
+                  msgDeliveryMode = Just Persistent
+                }
+          pure ()
+
+        it "cannot read a message if none have been published" $ \conn -> do
+          chan <- openChannel conn
+
+          -- declare a queue, exchange and binding
+          declareQueue chan newQueue {queueName = "myQueue"}
+          declareExchange chan newExchange {exchangeName = "myExchange", exchangeType = "direct"}
+          bindQueue chan "myQueue" "myExchange" "myKey"
+
+          -- Don't publish anything
+          mMesg <- getMsg chan Ack "myQueue"
+          case mMesg of
+            Nothing -> pure ()
+            Just (_, _) -> expectationFailure "Should not have been able to read any message."

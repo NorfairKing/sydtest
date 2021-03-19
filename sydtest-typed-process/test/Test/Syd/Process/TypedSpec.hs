935ff1887ff66eaa8beea31ccc72af2defb75bf3
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-03-19 12:35:54 +0100

    turn of typed-process tests because of https://github.com/fpco/typed-process/issues/38

--- sydtest-typed-process/test/Test/Syd/Process/TypedSpec.hs
+++ sydtest-typed-process/test/Test/Syd/Process/TypedSpec.hs
@@ -2,13 +2,23 @@
 
 module Test.Syd.Process.TypedSpec (spec) where
 
-import Control.Monad.STM
+import qualified Data.ByteString as SB
+import System.IO
 import System.Process.Typed
 import Test.Syd
 import Test.Syd.Process.Typed
 
 spec :: Spec
-spec = typedProcessSpec (setStdout byteStringOutput $ shell "echo hi") $ do
-  it "can communicate with the process" $ \ph -> do
-    stdout <- atomically $ getStdout ph
-    stdout `shouldBe` "hi\n"
+spec = typedProcessSpec (setStdin createPipe $ setStdout createPipe $ setStderr inherit $ proc "cat" []) $ do
+  -- This test fails, see
+  -- https://github.com/fpco/typed-process/issues/38
+  xit "can communicate with the process" $ \ph -> do
+    let inHandle = getStdin ph
+    let outHandle = getStdout ph
+    let contents = "Hello world"
+    hSetBuffering inHandle NoBuffering
+    hSetBuffering outHandle NoBuffering
+    SB.hPut inHandle contents
+    hFlush inHandle
+    output <- SB.hGet outHandle (SB.length contents)
+    output `shouldBe` contents

9e18db33fdfacfb9ca2ebdbba2e1f91997053268
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-03-20 16:28:35 +0100

    try out haskell.nix

--- default.nix
+++ default.nix
@@ -1,4 +1,13 @@
 let
-  pkgs = import ./nix/pkgs.nix { };
+  sources = import ./nix/sources.nix;
+  haskellNix = import sources."haskell.nix" { };
+  pkgsf = import sources.nixpkgs;
+  pkgs = pkgsf haskellNix.nixpkgsArgs;
 in
-pkgs.sydtestRelease
+pkgs.haskell-nix.project {
+  # 'cleanGit' cleans a source directory based on the files known by git
+  src = pkgs.haskell-nix.haskellLib.cleanGit {
+    name = "sydtest-project";
+    src = ./.;
+  };
+}

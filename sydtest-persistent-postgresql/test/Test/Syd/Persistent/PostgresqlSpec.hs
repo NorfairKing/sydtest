f28ecec71dcbe738a7a301ebd193db73b5553b26
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-03-22 16:35:34 +0100

    shared data test for the persistent companion libraries

--- sydtest-persistent-postgresql/test/Test/Syd/Persistent/PostgresqlSpec.hs
+++ sydtest-persistent-postgresql/test/Test/Syd/Persistent/PostgresqlSpec.hs
@@ -1,15 +1,27 @@
 module Test.Syd.Persistent.PostgresqlSpec (spec) where
 
 import Database.Persist
+import Database.Persist.Sql
 import Test.Syd
 import Test.Syd.Persistent.Example
 import Test.Syd.Persistent.Postgresql
 
 spec :: Spec
-spec = persistPostgresqlSpec migrateExample $
-  it "can write and read this example person" $ \pool ->
-    runPostgresqlTest pool $ do
-      let p = Person {personName = "John Doe", personAge = Just 21}
-      i <- insert p
-      mp <- get i
-      liftIO $ mp `shouldBe` Just p
+spec =
+  describe "persistPostgresqlSpec" $ do
+    persistPostgresqlSpec migrateExample $ do
+      it "can write and read this example person" $ \pool ->
+        runPostgresqlTest pool $ do
+          let p = Person {personName = "John Doe", personAge = Just 21}
+          i <- insert p
+          mp <- get i
+          liftIO $ mp `shouldBe` Just p
+      describe "shared data" $ do
+        it "can write this example person" $ \pool ->
+          runPostgresqlTest pool $ do
+            let p = Person {personName = "John Doe", personAge = Just 21}
+            insert_ p
+        it "cannot read anything that has not been written yet" $ \pool ->
+          runPostgresqlTest pool $ do
+            mp <- get (toSqlKey 0)
+            liftIO $ mp `shouldBe` (Nothing :: Maybe Person)
--- sydtest-persistent-sqlite/test/Test/Syd/Persistent/SqliteSpec.hs
+++ sydtest-persistent-sqlite/test/Test/Syd/Persistent/SqliteSpec.hs
@@ -1,15 +1,27 @@
 module Test.Syd.Persistent.SqliteSpec (spec) where
 
 import Database.Persist
+import Database.Persist.Sql
 import Test.Syd
 import Test.Syd.Persistent.Example
 import Test.Syd.Persistent.Sqlite
 
 spec :: Spec
-spec = persistSqliteSpec migrateExample $
-  it "can write and read this example person" $ \pool ->
-    runSqliteTest pool $ do
-      let p = Person {personName = "John Doe", personAge = Just 21}
-      i <- insert p
-      mp <- get i
-      liftIO $ mp `shouldBe` Just p
+spec =
+  describe "persistSqliteSpec" $ do
+    persistSqliteSpec migrateExample $ do
+      it "can write and read this example person" $ \pool ->
+        runSqliteTest pool $ do
+          let p = Person {personName = "John Doe", personAge = Just 21}
+          i <- insert p
+          mp <- get i
+          liftIO $ mp `shouldBe` Just p
+      describe "shared data" $ do
+        it "can write this example person" $ \pool ->
+          runSqliteTest pool $ do
+            let p = Person {personName = "John Doe", personAge = Just 21}
+            insert_ p
+        it "cannot read anything that has not been written yet" $ \pool ->
+          runSqliteTest pool $ do
+            mp <- get (toSqlKey 0)
+            liftIO $ mp `shouldBe` (Nothing :: Maybe Person)

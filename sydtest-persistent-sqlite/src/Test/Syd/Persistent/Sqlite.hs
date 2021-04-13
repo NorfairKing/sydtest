7e4e658dfe55ec6e77972ef487f3b8e6ba39d4d7
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-04-13 08:35:02 +0200

    usage example for persistent-sqlite

--- sydtest-persistent-sqlite/src/Test/Syd/Persistent/Sqlite.hs
+++ sydtest-persistent-sqlite/src/Test/Syd/Persistent/Sqlite.hs
@@ -9,6 +9,7 @@ module Test.Syd.Persistent.Sqlite
     connectionPoolSetupFunc',
     runSqlPool,
     runSqliteTest,
+    migrationRunner,
   )
 where
 
@@ -20,7 +21,31 @@ import Test.Syd
 
 -- | Declare a test suite that uses a database connection.
 --
--- This sets up the database connection around every test, so state is not preserved accross tests.
+-- Example usage
+--
+--
+-- > -- Database Definition
+-- > share
+-- >   [mkPersist sqlSettings, mkMigrate "migrateExample"]
+-- >   [persistLowerCase|
+-- > Person
+-- >     name String
+-- >     age Int Maybe
+-- >     deriving Show Eq
+-- > |]
+-- >
+-- > -- Tests
+-- > spec :: Spec
+-- > spec =
+-- >   persistSqliteSpec migrateExample $ do
+-- >     it "can write and read this example person" $ \pool ->
+-- >       runSqliteTest pool $ do
+-- >         let p = Person {personName = "John Doe", personAge = Just 21}
+-- >         i <- insert p
+-- >         mp <- get i
+-- >         liftIO $ mp `shouldBe` Just p
+--
+-- This sets up an in-memory database connection around every test, so state is not preserved accross tests.
 persistSqliteSpec :: Migration -> SpecWith ConnectionPool -> SpecWith a
 persistSqliteSpec migration = aroundWith $ \func _ -> withConnectionPool migration func
 
@@ -40,6 +65,11 @@ connectionPoolSetupFunc' = SetupFunc $ \takeConnectionPool migration ->
       _ <- flip runSqlPool pool $ migrationRunner migration
       liftIO $ takeConnectionPool pool
 
+-- | A flipped version of 'runSqlPool' to run your tests
+runSqliteTest :: ConnectionPool -> SqlPersistT IO a -> IO a
+runSqliteTest = flip runSqlPool
+
+-- | Helper function that works accross versions of @persistent@.
 #if MIN_VERSION_persistent(2,10,2)
 migrationRunner :: MonadIO m => Migration -> ReaderT SqlBackend m ()
 migrationRunner = void . runMigrationQuiet
@@ -47,7 +77,3 @@ migrationRunner = void . runMigrationQuiet
 migrationRunner :: MonadIO m => Migration -> ReaderT SqlBackend m ()
 migrationRunner = runMigration
 #endif
-
--- | A flipped version of 'runSqlPool' to run your tests
-runSqliteTest :: ConnectionPool -> SqlPersistT IO a -> IO a
-runSqliteTest = flip runSqlPool

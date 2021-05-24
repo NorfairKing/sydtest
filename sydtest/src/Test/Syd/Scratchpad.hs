5996689e3adfda06e8228e71d4b9662683318dba
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-05-24 21:31:13 +0200

    fixed the scratchpad

--- sydtest/src/Test/Syd/Scratchpad.hs
+++ sydtest/src/Test/Syd/Scratchpad.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
@@ -13,37 +14,35 @@ import Test.QuickCheck.Gen
 import Test.QuickCheck.Random
 
 data Property i where
-  Leaf :: i -> Property i
-  WithResource :: (forall r. (a -> IO r) -> IO r) -> (a -> Property i) -> Property i
+  Leaf :: (i -> IO ()) -> Property i
   ForAll :: Show a => Gen a -> (a -> [a]) -> (a -> Property i) -> Property i
 
-class Testable prop where
-  property :: prop -> Property (IO ())
+class Testable prop i where
+  property :: prop -> Property i
 
-instance Testable (Property (IO ())) where
+instance Testable (Property i) i where
   property = id
 
-instance Testable (IO ()) where
-  property = Leaf
+instance Testable (IO ()) i where
+  property = Leaf . const
 
-instance (Show a, Arbitrary a, Testable prop) => Testable (a -> prop) where
+instance (Show a, Arbitrary a, Testable prop i) => Testable (a -> prop) i where
   property p = ForAll arbitrary shrink $ \a -> property (p a)
 
-forAllShrink :: (Show a, Testable prop) => Gen a -> (a -> [a]) -> (a -> prop) -> Property (IO ())
+instance {-# OVERLAPPING #-} Testable (i -> IO ()) i where
+  property = Leaf
+
+forAllShrink :: (Show a, Testable prop i) => Gen a -> (a -> [a]) -> (a -> prop) -> Property i
 forAllShrink gen shrinker func = ForAll gen shrinker (property . func)
 
-forAll :: (Show a, Testable prop) => Gen a -> (a -> prop) -> Property (IO ())
+forAll :: (Show a, Testable prop i) => Gen a -> (a -> prop) -> Property i
 forAll g p = forAllShrink g (const []) p
-
-wrapProperty :: Testable prop => (forall r. (a -> IO r) -> IO r) -> (a -> prop) -> Property (IO ())
-wrapProperty wrapper p = WithResource wrapper (\a -> property (p a))
 
 -- | The results of one example being tested, with all the necessary information to do shrinking still there.
 --
 -- We'll have to shrink if the bottom bool is False
 data SingleResultTree i where
-  LeafResult :: Bool -> SingleResultTree i
-  WrapperResult :: Seed -> Size -> (forall r. (a -> IO r) -> IO r) -> (a -> Property i) -> SingleResultTree i -> SingleResultTree i
+  LeafResult :: (i -> IO ()) -> Bool -> SingleResultTree i
   ForAllResult :: Show a => Seed -> Size -> a -> (a -> [a]) -> (a -> Property i) -> SingleResultTree i -> SingleResultTree i
 
 type Seed = QCGen
@@ -60,51 +59,38 @@ didItCrash action = do
                 Handler (\(e :: SomeException) -> print e >> return False)
               ]
 
-runSingleExample :: Testable prop => Seed -> Size -> prop -> IO (SingleResultTree (IO ()))
-runSingleExample seed size prop = do
-  let go = \case
-        Leaf io -> LeafResult <$> didItCrash io
-        WithResource wrapper propFunc ->
-          wrapper $ \a -> WrapperResult seed size wrapper propFunc <$> go (propFunc a)
+runSingleExample :: forall i. Seed -> Size -> (forall r. (i -> IO r) -> IO r) -> Property i -> IO (SingleResultTree i)
+runSingleExample seed size wrapper prop = do
+  let go :: Property i -> IO (SingleResultTree i)
+      go = \case
+        Leaf io -> LeafResult io <$> didItCrash (wrapper io)
         ForAll gen shrinker propFunc -> do
           let generatedValue = unGen gen seed size
           ForAllResult seed size generatedValue shrinker propFunc <$> go (propFunc generatedValue)
-
-  go (property prop)
+  go prop
 
 didFail :: SingleResultTree i -> Bool
 didFail = go
   where
     go = \case
-      LeafResult b -> not b
-      WrapperResult _ _ _ _ srt -> go srt
+      LeafResult _ b -> not b
       ForAllResult _ _ _ _ _ srt -> go srt
 
 -- This could be adapted to return result that contains the number of shrinks, the failing example, etc.
-runShrunkExampleBecauseTheTestFailed :: SingleResultTree (IO ()) -> IO Bool
-runShrunkExampleBecauseTheTestFailed srt_ = do
+runShrunkExampleBecauseTheTestFailed :: (forall r. (i -> IO r) -> IO r) -> SingleResultTree i -> IO Bool
+runShrunkExampleBecauseTheTestFailed wrapper srt_ = do
   putStrLn "Starting to shrink"
   go srt_
   where
     go = \case
-      LeafResult r -> pure r
-      WrapperResult seed size wrapper propFunc _originalResult -> do
-        -- HERE BE THE DRAGONS
-        --
-        -- What the hell do we do here?
-        -- If we don't rerun the wrapper then the invariants of the wrappers are not maintained
-        -- If we do rerun the wrapper then all of the shrinking below is thrown away.
-        putStrLn "Running the wrapper while shrinking"
-        wrapper $ \a -> do
-          srt <- runSingleExample seed size (propFunc a)
-          go srt
+      LeafResult io _ -> didItCrash (wrapper io)
       ForAllResult seed size generatedValue shrinker propFunc originalResult -> do
         let go2 [] = do
               print ("return the failure", generatedValue)
               go originalResult
             go2 (smallerVersion : rest) = do
               print ("Trying smaller version", smallerVersion)
-              srt <- runSingleExample seed size (propFunc smallerVersion)
+              srt <- runSingleExample seed size wrapper (propFunc smallerVersion)
               success <- go srt
               print ("result", success)
               if success
@@ -114,13 +100,13 @@ runShrunkExampleBecauseTheTestFailed srt_ = do
                   go $ ForAllResult seed size smallerVersion shrinker propFunc srt
         go2 (shrinker generatedValue)
 
-runPropTest :: Testable prop => prop -> IO Bool
-runPropTest p = do
+runPropTest :: (forall r. (i -> IO r) -> IO r) -> Property i -> IO Bool
+runPropTest wrapper p = do
   seed <- newQCGen
   putStrLn "Starting the test"
-  srt <- runSingleExample seed 30 p
+  srt <- runSingleExample seed 30 wrapper p
   if didFail srt
-    then runShrunkExampleBecauseTheTestFailed srt
+    then runShrunkExampleBecauseTheTestFailed wrapper srt
     else pure True
 
 main :: IO ()
@@ -133,11 +119,11 @@ main = do
         atomically $ writeTVar var False
         pure r
 
-  b <- runPropTest $
-    wrapProperty withTrue $ \() -> forAllShrink (sized $ \n -> pure n) shrink $ \i -> do
+  b <- runPropTest withTrue $
+    forAllShrink (sized $ \n -> pure n) shrink $ \i () -> do
       print i
       b <- readTVarIO var
       if b
-        then if (i < 3) then pure () else error "test failed"
+        then if i < 3 then pure () else error "test failed"
         else error "Invariant violated." -- this should never happen
   print b

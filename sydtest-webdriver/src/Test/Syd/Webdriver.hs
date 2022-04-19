23468732e9f4b21f93f11e7f111cb2078fdd9706
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-04-19 14:31:47 +0200

    Imported sydtest-webdriver from what I already had

---
+++ sydtest-webdriver/src/Test/Syd/Webdriver.hs
@@ -0,0 +1,304 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+-- Because of webdriver using dangerous constructors
+{-# OPTIONS_GHC -fno-warn-incomplete-record-updates #-}
+-- For the undefined trick
+{-# OPTIONS_GHC -fno-warn-unused-pattern-binds #-}
+
+module Test.Syd.Webdriver where
+
+import Codec.Picture as Picture
+import Control.Arrow
+import Control.Monad.Base
+import Control.Monad.Reader
+import Control.Monad.Trans.Control
+import Data.Aeson as JSON
+import qualified Data.ByteString as SB
+import qualified Data.ByteString.Builder as BB
+import qualified Data.ByteString.Lazy as LB
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as TE
+import GHC.Stack
+import Network.HTTP.Client as HTTP
+import qualified Network.HTTP.Types as HTTP
+import Network.Socket
+import Network.Socket.Free
+import Network.Socket.Wait as Port
+import Network.URI
+import Path
+import Path.IO
+import System.Exit
+import System.Process.Typed
+import Test.Syd
+import Test.Syd.Path
+import Test.Syd.Process.Typed
+import Test.Syd.Wai
+import Test.Syd.Yesod
+import Test.WebDriver as WD hiding (setWindowSize)
+import Test.WebDriver.Class (WebDriver (..))
+import qualified Test.WebDriver.Commands.Internal as WD
+import qualified Test.WebDriver.JSON as WD
+import Test.WebDriver.Session (WDSessionState (..))
+import qualified Yesod
+
+newtype WebdriverTestM app a = WebdriverTestM
+  { unWebdriverTestM :: ReaderT (WebdriverTestEnv app) WD a
+  }
+  deriving
+    ( Functor,
+      Applicative,
+      Monad,
+      MonadIO,
+      MonadReader (WebdriverTestEnv app),
+      -- We don't want 'MonadBaseControl IO' or 'MonadBase IO', but we have to
+      -- because webdriver uses them.
+      MonadBaseControl IO,
+      MonadBase IO
+    )
+
+data WebdriverTestEnv app = WebdriverTestEnv
+  { -- | The base url of the app we test, so that we can test external sites just like local ones.
+    webdriverTestEnvURI :: !URI,
+    -- | The webdriver configuration
+    webdriverTestEnvConfig :: !WDConfig,
+    -- | The Yesod app that we'll test.
+    webdriverTestEnvApp :: !app
+  }
+
+instance WDSessionState (WebdriverTestM app) where
+  getSession = WebdriverTestM getSession
+  putSession = WebdriverTestM . putSession
+
+instance WebDriver (WebdriverTestM app) where
+  doCommand m p a = WebdriverTestM $ doCommand m p a
+
+instance IsTest (WebdriverTestM app ()) where
+  type Arg1 (WebdriverTestM app ()) = ()
+  type Arg2 (WebdriverTestM app ()) = WebdriverTestEnv app
+  runTest wdTestFunc = runTest (\() wdte -> runWebdriverTestM wdte wdTestFunc)
+
+instance IsTest (WebdriverTestM app (GoldenTest a)) where
+  type Arg1 (WebdriverTestM app (GoldenTest a)) = ()
+  type Arg2 (WebdriverTestM app (GoldenTest a)) = WebdriverTestEnv app
+  runTest wdTestFunc = runTest (\() wdte -> runWebdriverTestM wdte wdTestFunc)
+
+runWebdriverTestM :: WebdriverTestEnv app -> WebdriverTestM app a -> IO a
+runWebdriverTestM env (WebdriverTestM func) = WD.runSession (webdriverTestEnvConfig env) $
+  WD.finallyClose $ do
+    setImplicitWait 10_000
+    setScriptTimeout 10_000
+    setPageLoadTimeout 10_000
+    runReaderT func env
+
+openRoute ::
+  Yesod.RenderRoute app =>
+  Route app ->
+  WebdriverTestM app ()
+openRoute route = openRouteWithParams route []
+
+openRouteWithParams ::
+  Yesod.RenderRoute app =>
+  Route app ->
+  [(Text, Text)] ->
+  WebdriverTestM app ()
+openRouteWithParams route extraParams = do
+  uri <- asks webdriverTestEnvURI
+  let (pathPieces, queryParams) = Yesod.renderRoute route
+  let q = queryTextToQuery $ map (second Just) (queryParams <> extraParams)
+  let pathBS = encodePath pathPieces q
+  case TE.decodeUtf8' (LB.toStrict (BB.toLazyByteString pathBS)) of
+    Left err -> liftIO $ die $ show err
+    Right t -> openPage $ show uri <> T.unpack t
+
+getCurrentRoute ::
+  Yesod.ParseRoute app =>
+  WebdriverTestM app (Route app)
+getCurrentRoute = do
+  currentUrl <- getCurrentURL
+  case parseURI currentUrl of
+    Nothing -> liftIO $ expectationFailure $ "Should have been able to parse the current url into an URI: " <> currentUrl
+    Just URI {..} -> do
+      let (textPieces, query_) = HTTP.decodePath $ TE.encodeUtf8 $ T.pack $ concat [uriPath, uriQuery]
+          queryPieces = map unJust $ HTTP.queryToQueryText query_
+      case Yesod.parseRoute (textPieces, queryPieces) of
+        Nothing ->
+          liftIO $
+            expectationFailure $
+              unlines
+                [ "Should have been able to parse an App route from " <> currentUrl,
+                  ppShow (textPieces, queryPieces)
+                ]
+        Just route -> pure route
+  where
+    unJust (a, Just b) = (a, b)
+    unJust (a, Nothing) = (a, "")
+
+type WebdriverSpec app = TestDef '[SeleniumServerHandle, HTTP.Manager] (WebdriverTestEnv app)
+
+webdriverSpec ::
+  Yesod.YesodDispatch app =>
+  (HTTP.Manager -> SetupFunc app) ->
+  WebdriverSpec app ->
+  Spec
+webdriverSpec appSetupFunc =
+  managerSpec
+    . modifyMaxSuccess (`div` 50)
+    . yesodSpecWithSiteSetupFunc appSetupFunc
+    . setupAroundAll seleniumServerSetupFunc
+    . webdriverTestEnvSpec
+
+webdriverTestEnvSpec ::
+  TestDef '[SeleniumServerHandle, HTTP.Manager] (WebdriverTestEnv app) ->
+  TestDef '[SeleniumServerHandle, HTTP.Manager] (YesodClient app)
+webdriverTestEnvSpec = setupAroundWith' go2 . setupAroundWith' go1
+  where
+    go1 ::
+      SeleniumServerHandle ->
+      (SeleniumServerHandle -> SetupFunc (WebdriverTestEnv app)) ->
+      SetupFunc (WebdriverTestEnv app)
+    go1 ssh func = func ssh
+    go2 ::
+      HTTP.Manager ->
+      YesodClient app ->
+      SetupFunc (SeleniumServerHandle -> SetupFunc (WebdriverTestEnv app))
+    go2 man yc = pure $ \ssh -> webdriverTestEnvSetupFunc ssh man yc
+
+webdriverTestEnvSetupFunc ::
+  SeleniumServerHandle ->
+  HTTP.Manager ->
+  YesodClient app ->
+  SetupFunc (WebdriverTestEnv app)
+webdriverTestEnvSetupFunc SeleniumServerHandle {..} manager YesodClient {..} = do
+  chromeExecutable <- liftIO $ do
+    chromeFile <- parseRelFile "chromium"
+    mExecutable <- findExecutable chromeFile
+    case mExecutable of
+      Nothing -> die "No chromium found on PATH."
+      Just executable -> pure executable
+
+  userDataDir <- tempDirSetupFunc "chromium-user-data"
+
+  let browser =
+        chrome
+          { chromeOptions =
+              [ "--user-data-dir=" <> fromAbsDir userDataDir,
+                "--headless",
+                "--no-sandbox", -- Bypass OS security model to run on nix as well
+                "--disable-dev-shm-usage", -- Overcome limited resource problem
+                "--disable-gpu",
+                "--use-gl=angle",
+                "--use-angle=swiftshader",
+                "--window-size=1920,1080"
+              ],
+            chromeBinary = Just $ fromAbsFile chromeExecutable
+          }
+  let caps =
+        WD.defaultCaps
+          { browser = browser
+          }
+  let webdriverTestEnvConfig =
+        WD.defaultConfig
+          { wdPort = (fromIntegral :: PortNumber -> Int) seleniumServerHandlePort,
+            wdHTTPManager = Just manager,
+            wdCapabilities = caps
+          }
+  let webdriverTestEnvURI = yesodClientSiteURI
+      webdriverTestEnvApp = yesodClientSite
+  pure WebdriverTestEnv {..}
+
+seleniumServerSetupFunc :: SetupFunc SeleniumServerHandle
+seleniumServerSetupFunc = do
+  tempDir <- tempDirSetupFunc "selenium-server"
+  portInt <- liftIO getFreePort
+  let processConfig =
+        setStdout nullStream $
+          setStderr nullStream $
+            setWorkingDir (fromAbsDir tempDir) $
+              proc
+                "selenium-server"
+                [ "-port",
+                  show portInt
+                ]
+  _ <- typedProcessSetupFunc processConfig
+  liftIO $ Port.wait "127.0.0.1" portInt
+  let seleniumServerHandlePort = fromIntegral portInt
+  pure SeleniumServerHandle {..}
+
+data SeleniumServerHandle = SeleniumServerHandle
+  { seleniumServerHandlePort :: PortNumber
+  }
+
+data Screenshot = Screenshot
+  { screenshotFile :: !(Path Abs File),
+    screenshotImage :: !(Picture.Image PixelRGB8)
+  }
+
+pureGoldenScreenshot :: FilePath -> LB.ByteString -> GoldenTest Screenshot
+pureGoldenScreenshot fp contents =
+  GoldenTest
+    { goldenTestRead = do
+        relFile <- parseRelFile fp
+        currentDir <- getCurrentDir
+        let resolvedFile = currentDir </> relFile
+        mContents <- forgivingAbsence $ SB.readFile $ fromAbsFile resolvedFile
+        forM mContents $ \cts -> do
+          case decodePng cts of
+            Left err -> die err
+            Right dynamicImage ->
+              pure $
+                Screenshot
+                  { screenshotFile = resolvedFile,
+                    screenshotImage = convertRGB8 dynamicImage
+                  },
+      goldenTestProduce = do
+        let sb = LB.toStrict contents
+        case decodePng sb of
+          Left err -> expectationFailure $ "Could not parse screenshot as png: " <> err
+          Right dynamicImage -> do
+            let image = convertRGB8 dynamicImage
+            relFile <- parseRelFile fp
+            tempDir <- resolveDir' "screenshot-comparison"
+            let tempFile = tempDir </> relFile
+            ensureDir $ parent tempFile
+            -- Write it to a file so we can compare it if it differs.
+            writePng (fromAbsFile tempFile) image
+            pure $
+              Screenshot
+                { screenshotFile = tempFile,
+                  screenshotImage = image
+                },
+      goldenTestWrite = \(Screenshot _ actual) -> do
+        relFile <- parseRelFile fp
+        currentDir <- getCurrentDir
+        let resolvedFile = currentDir </> relFile
+        ensureDir $ parent resolvedFile
+        writePng (fromAbsFile resolvedFile) actual,
+      goldenTestCompare = \(Screenshot actualPath actual) (Screenshot expectedPath expected) ->
+        if actual == expected
+          then Nothing
+          else
+            Just $
+              ExpectationFailed $
+                unlines
+                  [ "Screenshots differ.",
+                    "expected: " <> fromAbsFile expectedPath,
+                    "actual: " <> fromAbsFile actualPath
+                  ]
+    }
+
+-- We have to override this because it returns something.
+-- So we remove the 'noReturn'.
+setWindowSize :: (HasCallStack, WebDriver wd) => (Word, Word) -> wd ()
+setWindowSize (w, h) =
+  WD.ignoreReturn $
+    WD.doWinCommand methodPost currentWindow "/size" $
+      object ["width" .= w, "height" .= h]

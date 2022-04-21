ea9eaa3234042d4a653e8f088827f01b86f6f995
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-04-21 11:04:27 +0200

    passthru an enableMongo fix

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -19,6 +19,36 @@ with final.haskell.lib;
         exeName: name:
         generateOptparseApplicativeCompletion exeName (sydtestPkg name);
       sydtestPkgWithOwnComp = name: sydtestPkgWithComp name name;
+
+      enableMongo = haskellPkg: overrideCabal haskellPkg (old: {
+        buildDepends = (old.buildDepends or [ ]) ++ [ final.mongodb ];
+        # The mongodb library uses network-bsd's function getProtocolByName
+        # to lookup the port that corresponds to the tcp protocol:
+        # https://hackage.haskell.org/package/network-bsd-2.8.1.0/docs/Network-BSD.html#v:getProtocolByName
+        # This consults a system database that is in /etc/protocols, see
+        # https://linux.die.net/man/3/getprotobyname and
+        # https://linux.die.net/man/5/protocols
+        #
+        # However, this file doesn't exist in the nix sandbox, so we need to
+        # somehowe make the test suite think that it does.
+        #
+        # There's no way to put something at /etc/protocols in the test suite,
+        # but we can use LD_PRELOAD to use libredirect to replace the openat
+        # glibc calls that the test suite makes by openat calls that look for a
+        # different filename.
+        #
+        # This mapping from expected filename to actual filename is given in a
+        # NIX_REDIRECTS environment variable, and the /etc/protocols file that
+        # we want to use is in iana-etc.
+        preCheck = (old.preCheck or "") + ''
+          export NIX_REDIRECTS=/etc/protocols=${final.iana-etc}/etc/protocols
+          export LD_PRELOAD=${final.libredirect}/lib/libredirect.so
+        '';
+        postCheck = (old.postCheck or "") + ''
+          unset NIX_REDIRECTS LD_PRELOAD
+        '';
+      });
+
       fontsConfig = final.callPackage ./fonts-conf.nix { };
       setupFontsConfigScript = ''
         export FONTCONFIG_SYSROOT=${fontsConfig}
@@ -61,33 +91,10 @@ with final.haskell.lib;
       "sydtest-persistent-postgresql" = overrideCabal (sydtestPkg "sydtest-persistent-postgresql") (old: {
         testDepends = (old.testDepends or [ ]) ++ [ final.postgresql ];
       });
-      "sydtest-mongo" = overrideCabal (sydtestPkg "sydtest-mongo") (old: {
-        buildDepends = (old.buildDepends or [ ]) ++ [ final.mongodb ];
-        # The mongodb library uses network-bsd's function getProtocolByName
-        # to lookup the port that corresponds to the tcp protocol:
-        # https://hackage.haskell.org/package/network-bsd-2.8.1.0/docs/Network-BSD.html#v:getProtocolByName
-        # This consults a system database that is in /etc/protocols, see
-        # https://linux.die.net/man/3/getprotobyname and
-        # https://linux.die.net/man/5/protocols
-        #
-        # However, this file doesn't exist in the nix sandbox, so we need to
-        # somehowe make the test suite think that it does.
-        # 
-        # There's no way to put something at /etc/protocols in the test suite,
-        # but we can use LD_PRELOAD to use libredirect to replace the openat
-        # glibc calls that the test suite makes by openat calls that look for a
-        # different filename.
-        #
-        # This mapping from expected filename to actual filename is given in a
-        # NIX_REDIRECTS environment variable, and the /etc/protocols file that
-        # we want to use is in iana-etc.
-        preCheck = (old.preCheck or "") + ''
-          export NIX_REDIRECTS=/etc/protocols=${final.iana-etc}/etc/protocols
-          export LD_PRELOAD=${final.libredirect}/lib/libredirect.so
-        '';
-        postCheck = (old.postCheck or "") + ''
-          unset NIX_REDIRECTS LD_PRELOAD
-        '';
+      "sydtest-mongo" = (enableMongo (sydtestPkg "sydtest-mongo")).overrideAttrs (old: {
+        passthru = {
+          inherit enableMongo;
+        };
       });
       "sydtest-webdriver" = (enableWebdriver (sydtestPkg "sydtest-webdriver")).overrideAttrs (old: {
         passthru = {

4bd8ad801c059a8f731c08a6f0171e2c097e589b
Tom Sydney Kerckhove <syd@cs-syd.eu>
2023-12-06 13:57:55 +0100

    Trying to fix the builds

--- sydtest-mongo/default.nix
+++ sydtest-mongo/default.nix
@@ -1,6 +1,6 @@
 { mkDerivation, base, binary, bson, bytestring, lib, mongoDB
 , network, path, path-io, port-utils, process, sydtest
-, sydtest-discover, sydtest-process, text, yaml
+, sydtest-process, text, yaml
 }:
 mkDerivation {
   pname = "sydtest-mongo";
@@ -10,8 +10,6 @@ mkDerivation {
     base binary bson bytestring mongoDB network path path-io port-utils
     process sydtest sydtest-process text yaml
   ];
-  testHaskellDepends = [ base mongoDB sydtest text ];
-  testToolDepends = [ sydtest-discover ];
   homepage = "https://github.com/NorfairKing/sydtest#readme";
   description = "An mongoDB companion library for sydtest";
   license = "unknown";

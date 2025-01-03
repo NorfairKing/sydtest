a4920c99c65adeddfb8aec6fed06dbd0a106e324
Sean D Gillespie <sean@mistersg.net>
2025-01-03 17:32:41 -0500

    test: Add a sydtest-hspec output test

--- sydtest-hspec/default.nix
+++ sydtest-hspec/default.nix
@@ -1,5 +1,5 @@
-{ mkDerivation, base, hspec, hspec-core, lib, mtl, QuickCheck, stm
-, sydtest, sydtest-discover
+{ mkDerivation, base, hspec, hspec-core, lib, mtl, QuickCheck
+, safe-coloured-text, stm, sydtest, sydtest-discover, text
 }:
 mkDerivation {
   pname = "sydtest-hspec";
@@ -8,7 +8,9 @@ mkDerivation {
   libraryHaskellDepends = [
     base hspec-core mtl QuickCheck stm sydtest
   ];
-  testHaskellDepends = [ base hspec stm sydtest ];
+  testHaskellDepends = [
+    base hspec safe-coloured-text stm sydtest text
+  ];
   testToolDepends = [ sydtest-discover ];
   homepage = "https://github.com/NorfairKing/sydtest#readme";
   description = "An Hspec companion library for sydtest";

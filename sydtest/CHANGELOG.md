d2e3a5b1c4f5e09ce61519b4b60e51303acd8d15
Tom Sydney Kerckhove <syd@cs-syd.eu>
2026-02-26 13:20:20 +0100

    Bump version and release

--- sydtest/CHANGELOG.md
+++ sydtest/CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## [0.23.0.2] - 2026-02-26
+
+### Changed
+
+* Fixed a missing import on Windows.
 
 ## [0.23.0.1] - 2026-01-26
 
--- sydtest/default.nix
+++ sydtest/default.nix
@@ -6,7 +6,7 @@
 }:
 mkDerivation {
   pname = "sydtest";
-  version = "0.23.0.1";
+  version = "0.23.0.2";
   src = ./.;
   libraryHaskellDepends = [
     async autodocodec base bytestring containers deepseq dlist

f15dfb3ff44ec46577c076be7d2ab850676318b6
Tom Sydney Kerckhove <syd@cs-syd.eu>
2026-06-01 07:51:56 +0200

    Upgrade to nixpkgs version nixos-26.05 and lts 24.38

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     extra-trusted-public-keys = "sydtest.cachix.org-1:fyby3c42t+0iTABcLd/R3POxzJhCQ/9gYM7Sh879+9w=";
   };
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-26.05";
     horizon-advance.url = "git+https://gitlab.horizon-haskell.net/package-sets/horizon-advance";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
     autodocodec.url = "github:NorfairKing/autodocodec";
